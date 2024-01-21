946407d6110a38bd5c3af6032bf4eec5fdbe50e9
t1nk-r <janvari.gusztav@imprestige.biz>
2024-01-21 16:29:00 +0100

    Prepare for renaming

--- __init__.py
+++ __init__.py
@@ -1,22 +1,51 @@
-# Object Renamer
+# T1nk-R's Unified Rename add-on for Blender
+# - part of T1nk-R Utilities for Blender
 #
-# COPYRIGHT **************************************************************************************************
-# Creative Commons CC-BY-SA. Simply to put, you can create derivative works based on this script,
-# and if you are nice, you don't remove the following attribution:
+# This module is responsible for managing add-on and settings lifecycle.
 #
-# Original script created by: T1nk-R - na.mondhatod@gmail.com
-# Version 2021-05-15
+# Module and add-on authored by T1nk-R (https://github.com/gusztavj/)
 #
-# DISCLAIMER *************************************************************************************************
-# This script is provided as-is. Use at your own risk. No warranties, no guarantee, no liability,
-# no matter what happens. Still I tried to make sure no weird things happen.
+# PURPOSE & USAGE *****************************************************************************************************************
+# You can use this add-on to get a single panel to batch rename collections, objects and object data in Blender's Outliner
+# using text or regex-based search and replace.
 #
-# 
+# Help, support, updates and anything else: https://github.com/gusztavj/Custom-Object-Property-Manager
+#
+# COPYRIGHT ***********************************************************************************************************************
+# Creative Commons CC BY-NC-SA:
+#       This license enables re-users to distribute, remix, adapt, and build upon the material in any medium 
+#       or format for noncommercial purposes only, and only so long as attribution is given to the creator. 
+#       If you remix, adapt, or build upon the material, you must license the modified material under 
+#       identical terms. CC BY-NC-SA includes the following elements:
+#           BY: credit must be given to the creator.
+#           NC: Only noncommercial uses of the work are permitted.
+#           SA: Adaptations must be shared under the same terms.
+#
+#       Credit text:
+#           Original addon created by: T1nk-R - janvari.gusztav@imprestige.biz
+#
+#       For commercial use, please contact me via janvari.gusztav@imprestige.biz. Don't be scared of
+#       rigid contracts and high prices, above all I just want to know if this work is of your interest,
+#       and discuss options for commercial support and other services you may need.
+#
+#
+# Version: Please see the version tag under bl_info below.
+#
+# DISCLAIMER **********************************************************************************************************************
+# This add-on is provided as-is. Use at your own risk. No warranties, no guarantee, no liability,
+# no matter what happens. Still I tried to make sure no weird things happen:
+#   * This add-on is intended to change the name of your Blender objects, collections and data blocks belonging to objects.
+#   * This add-on is not intended to modify your objects and other Blender assets in any other way.
+#   * You shall be able to simply undo consequences made by this add-on.
+#
+# You may learn more about legal matters on page https://github.com/gusztavj/Custom-Object-Property-Manager
+#
+# *********************************************************************************************************************************
 
 bl_info = {
     "name": "T1nk-R Collection Renamer",
     "author": "GusJ",
-    "version": (1, 0),
+    "version": (1, 0, 0),
     "blender": (2, 91, 0),
     "location": "Edit",
     "description": "Rename your collections",
